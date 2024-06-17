d5990901ce7836f8520705cd6c8a707723aa61b6
Purevbat <puba0115@gmail.com>
2024-06-17 23:42:49 +0800

    init33

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,46 @@
-import React, {Component} from 'react';
-
+import React, { Component } from 'react';
 import Header from './Components/Header/Header';
 import ChatHistory from './Components/ChatHistory/ChatHistory';
 import ChatInput from './Components/ChatInput/ChatInput';
 import './App.css';
 
-import {connect, sendMsg} from './API';
+import { connect, sendMsg } from './API';
 
-class App extends Component{
-
-  constructor(props){
-    super(props);
-    this.state = {
-      ChatHistory: []
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            chatHistory: []
+        };
     }
-  }
-  componentDidMount(){
-    connect((msg) =>{
-        console.log("New Message") 
-        this.setState(prevState => ({
-           ChatHistory : [...prevState.chatHistory, msg]
-        }))
-        console.log(this.state);
-    })
-  }
 
-  render(){
-    return(
-      <div className='App'>
-        <Header/>
-        <ChatHistory chatHistory = {this.state.chatHistory}/>
-        <ChatInput send = {this.send}/> 
-      </div>
-    );
-  }
+    componentDidMount() {
+        connect((msg) => {
+            console.log("New Message");
+            this.setState(prevState => ({
+                chatHistory: [...prevState.chatHistory, msg] 
+            }));
+            console.log(this.state);
+        });
+    }
 
+    // Function to send message
+    send = (event) => {
+        if (event.key === "Enter") {
+            sendMsg(event.target.value);
+            event.target.value = "";
+        }
+    }
+
+    render() {
+        return (
+            <div className='App'>
+                <Header />
+                <ChatHistory chatHistory={this.state.chatHistory} /> {/* Corrected prop name */}
+                <ChatInput send={this.send} /> {/* Pass the send function */}
+            </div>
+        );
+    }
 }
 
 export default App;
